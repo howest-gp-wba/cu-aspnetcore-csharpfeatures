eb462f3d8ba3ed1bcfe2446835dab0137eecb5e4
sigged <sderdeyn@gmail.com>
2017-05-16 14:33:01 +0200

    Convert to task based asynchronous pattern with await/async

--- src/CoreCourse.CSharpFeatures.AsyncApp/Program.cs
+++ src/CoreCourse.CSharpFeatures.AsyncApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreCourse.CSharpFeatures.AsyncApp
 {
@@ -21,33 +22,37 @@ namespace CoreCourse.CSharpFeatures.AsyncApp
                 new { Url = "https://www.github.com", Rating=75 },
             };
 
-            Console.WriteLine($"Checking {siteDefinitions.Length} sites...\r\n ");
+            Console.WriteLine($"Checking {siteDefinitions.Length} sites...\r\n");
 
             //check sites
-            CheckSites(siteDefinitions.Select(e => e.Url));
+            Task checkTask = CheckSites(siteDefinitions.Select(e => e.Url));
 
-            //calculate average
+            //calculate average while checking sites
             var averageRating = sitechecker.AverageRating(siteDefinitions.Select(sd => sd.Rating));
             Console.WriteLine($"\r\nAverage rating: {averageRating:N2}\r\n");
+
+            //wait for task to finish
+            checkTask.Wait();
 
             //prevent quitting in debug mode
             Console.WriteLine($"\r\nPress any key to exit");
             Console.Read();
         }
 
-        static void CheckSites(IEnumerable<string> urls)
+        static async Task CheckSites(IEnumerable<string> urls)
         {
             ConsoleColor defaultColor = Console.ForegroundColor;
 
             //check sites
             foreach (var url in urls)
             {
-                var status = sitechecker.GetStatusCode(url);
+                var status = await sitechecker.GetStatusCode(url);
                 if (status != 200)
                     Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"{url} status: {status}");
                 Console.ForegroundColor = defaultColor;
             }
         }
+
     }
 }
--- src/CoreCourse.CSharpFeatures.AsyncApp/SiteChecker.cs
+++ src/CoreCourse.CSharpFeatures.AsyncApp/SiteChecker.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CoreCourse.CSharpFeatures.AsyncApp
 {
     public class SiteChecker
     {
-        public int GetStatusCode(string siteurl)
+        public async Task<int> GetStatusCode(string siteurl)
         {
             var client = new HttpClient();
-            var httpResponse = client.GetAsync(siteurl).Result;
+            var httpResponse = await client.GetAsync(siteurl);
             return (int)httpResponse.StatusCode;
         }
+
         public double AverageRating(IEnumerable<int> ratings)
         {
             return ratings.Average();
