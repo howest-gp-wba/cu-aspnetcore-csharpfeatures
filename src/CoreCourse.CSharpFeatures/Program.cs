a9ec3808d6ac6b6381f3b629e5cd9730579d6ae6
sigged <sderdeyn@gmail.com>
2017-05-16 02:55:14 +0200

    Created simple console application

---
+++ src/CoreCourse.CSharpFeatures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreCourse.CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
