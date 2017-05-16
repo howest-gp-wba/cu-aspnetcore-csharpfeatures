2aff4d19a3e82cbf64f327291ef9fdd7460e8448
sigged <sderdeyn@gmail.com>
2017-05-16 14:30:06 +0200

    Synchronously reading statuscodes by using anonymous type objects

--- src/CoreCourse.CSharpFeatures.AsyncApp/Program.cs
+++ src/CoreCourse.CSharpFeatures.AsyncApp/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreCourse.CSharpFeatures.AsyncApp
 {
     class Program
     {
+        static SiteChecker sitechecker = new SiteChecker();
+
         static void Main(string[] args)
         {
-            //collection initialization with anonymous types
+            //immediate initialization
             var siteDefinitions = new[]
             {
                 new { Url = "http://www.google.com", Rating=90 },
@@ -17,10 +21,33 @@ namespace CoreCourse.CSharpFeatures.AsyncApp
                 new { Url = "https://www.github.com", Rating=75 },
             };
 
+            Console.WriteLine($"Checking {siteDefinitions.Length} sites...\r\n ");
+
+            //check sites
+            CheckSites(siteDefinitions.Select(e => e.Url));
+
+            //calculate average
+            var averageRating = sitechecker.AverageRating(siteDefinitions.Select(sd => sd.Rating));
+            Console.WriteLine($"\r\nAverage rating: {averageRating:N2}\r\n");
+
             //prevent quitting in debug mode
             Console.WriteLine($"\r\nPress any key to exit");
             Console.Read();
         }
 
+        static void CheckSites(IEnumerable<string> urls)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+
+            //check sites
+            foreach (var url in urls)
+            {
+                var status = sitechecker.GetStatusCode(url);
+                if (status != 200)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{url} status: {status}");
+                Console.ForegroundColor = defaultColor;
+            }
+        }
     }
 }
