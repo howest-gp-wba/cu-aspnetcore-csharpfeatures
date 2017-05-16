a565a5b5fbb6e967db843f6e6fd59e38883943b9
sigged <sderdeyn@gmail.com>
2017-05-16 14:26:21 +0200

    Created new console app AsyncApp

---
+++ src/CoreCourse.CSharpFeatures.AsyncApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CoreCourse.CSharpFeatures.AsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection initialization with anonymous types
+            var siteDefinitions = new[]
+            {
+                new { Url = "http://www.google.com", Rating=90 },
+                new { Url = "https://www.stackoverflow.com", Rating=80 },
+                new { Url = "https://www.codeproject.com", Rating=75 },
+                new { Url = "http://www.deredactie.be", Rating=70 },
+                new { Url = "http://www.svt.se", Rating=70 },
+                new { Url = "https://www.github.com", Rating=75 },
+            };
+
+            //prevent quitting in debug mode
+            Console.WriteLine($"\r\nPress any key to exit");
+            Console.Read();
+        }
+
+    }
+}
---
+++ src/CoreCourse.CSharpFeatures.AsyncApp/SiteChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCourse.CSharpFeatures.AsyncApp
+{
+    public class SiteChecker
+    {
+        public int GetStatusCode(string siteurl)
+        {
+            var client = new HttpClient();
+            var httpResponse = client.GetAsync(siteurl).Result;
+            return (int)httpResponse.StatusCode;
+        }
+        public double AverageRating(IEnumerable<int> ratings)
+        {
+            return ratings.Average();
+        }
+    }
+}
